774e9a9aa62d2224ccfa54344c21fc60dc2ea260
rubby <rubbyhandojo03@gmail.com>
2021-07-16 14:44:10 +0700

    baru

---
+++ mc3-team8/mc3-team8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mc3-team8
+//
+//  Created by rubby handojo on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
