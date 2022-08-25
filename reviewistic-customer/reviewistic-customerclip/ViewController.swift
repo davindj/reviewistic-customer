1bb9004fe4c650be61ed876da949a65c9e99c328
Davin DJ <davindx88@gmail.com>
2022-08-25 19:28:03 +0700

    Fix Bug - immortal keyboard (unable to dismiss)

--- reviewistic-customer/reviewistic-customerclip/ViewController.swift
+++ reviewistic-customer/reviewistic-customerclip/ViewController.swift
@@ -40,6 +40,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     @IBAction func priceBtnTapped(_ sender: UIButton) {
